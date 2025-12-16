48a46767e887464f3840e09e2707b47bbe5b6a20
Jack11111eee <1820060304@qq.com>
2025-12-16 21:51:31 +0800

    adjust the structure of project

--- PestClassification/frontend/src/api/user.js
+++
@@ -1,9 +0,0 @@
-import api from "./axios";
-
-export function deleteUser(id) {
-  return api.delete(`/admin/users/${id}`);
-}
-
-export function getAllUsers() {
-  return api.get("/admin/users");
-}
--- PestClassification/frontend/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'//主组件
-import router from './router'//路由配置
-
-createApp(App).use(router).mount('#app')
